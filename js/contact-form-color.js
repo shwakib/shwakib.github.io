df3144799bf3e81a60bf6e68fe1afd610f209091
shwakib <sifat6460@gmail.com>
2022-05-08 16:33:13 +0600

    js added

---
+++ js/contact-form-color.js
@@ -0,0 +1,20 @@
+function contactname() {
+  var w = document.getElementById("nameofperson");
+  w.style.backgroundColor = 'white';
+  w.style.border = "solid #207398";
+}
+function contactemail() {
+  var x = document.getElementById("emailofperson");
+  x.style.backgroundColor = 'white';
+  x.style.border = "solid #207398";
+}
+function contactsubject() {
+  var y = document.getElementById("subjectofperson");
+  y.style.backgroundColor = 'white';
+  y.style.border = "solid #207398";
+}
+function contactmessage() {
+  var z = document.getElementById("messageofperson");
+  z.style.backgroundColor = 'white';
+  z.style.border = "solid #207398";
+}
---
+++ js/pre-loader.js
@@ -0,0 +1,4 @@
+var loader = document.getElementById("pre-loader");
+window.addEventListener("load", function () {
+    loader.style.display='none'
+})
